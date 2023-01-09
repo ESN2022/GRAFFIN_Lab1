651727497a47f953ece35a5d69b0a127ec096e8d
Maxence GRAFFIN <magraffin>
2023-01-09 16:31:48 +0100

    delet qsys genered files

--- Lab1_qsys/Lab1_qsys_inst.v
+++
@@ -1,7 +0,0 @@
-	Lab1_qsys u0 (
-		.clk_clk      (<connected-to-clk_clk>),      //   clk.clk
-		.pio_0_export (<connected-to-pio_0_export>), // pio_0.export
-		.rst_reset_n  (<connected-to-rst_reset_n>),  //   rst.reset_n
-		.pio_1_export (<connected-to-pio_1_export>)  // pio_1.export
-	);
-
